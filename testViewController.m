99c12ccb440caea5f6ebd9e7df39178e8010ba11
admin <admin@admindeMac-mini.local>
2016-12-15 12:45:58 +0800

    ooo
    ooo

--- testViewController.m
+++ testViewController.m
@@ -11,6 +11,8 @@
 
 @interface testViewController ()
 @property (nonatomic,strong) UIViewController *test2vc;
+
+@property (nonatomic,assign) BOOL isShow;
 @end
 
 @implementation testViewController
@@ -24,6 +26,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.isShow = NO;
     
     self.view.backgroundColor = [UIColor whiteColor];
     
@@ -87,33 +90,51 @@
 }
 
 - (void)test2:(UIButton *)sender{
+    
+
+
     NSURL *jsCodeLocation;
-    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main2" withExtension:@"jsbundle"];
+    
+    if (self.isShow) {
+      jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main3" withExtension:@"jsbundle"];
+    }else{
+      jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main2" withExtension:@"jsbundle"];
+    }
+
+    
     RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                         moduleName:@"AwesomeProject33"
                                                      launchOptions:nil];
-
     self.test2vc.view = rootView;
     UIButton *button2 = [UIButton buttonWithType:0];
     [button2 setTitle:@"热更新" forState:0];
     [button2 setBackgroundColor:[UIColor greenColor]];
     [button2 setTitleColor:[UIColor blackColor] forState:0];
     [button2 addTarget:self action:@selector(test3:) forControlEvents:UIControlEventTouchUpInside];
-    button2.frame = CGRectMake(80, 150, 100, 40);
+    button2.frame = CGRectMake(200, 80, 100, 40);
     button2.layer.cornerRadius = 20;
     button2.layer.masksToBounds = YES;
     [self.test2vc.view addSubview:button2];
     //  [self presentViewController:rootViewController animated:YES completion:nil];
     [self.navigationController pushViewController:self.test2vc animated:YES];
+    
+    
 }
 
 - (void)test3:(UIButton *)sender{
+    
+   
     NSURL *jsCodeLocation;
-    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    if (!self.isShow) {
+        jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main3" withExtension:@"jsbundle"];
+    }else{
+        jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main2" withExtension:@"jsbundle"];
+    }
     RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                         moduleName:@"AwesomeProject33"
                                                      launchOptions:nil];
     self.test2vc.view = rootView;
+    self.isShow = !self.isShow;
 }
 
 //远程服务器更改bundel文件
